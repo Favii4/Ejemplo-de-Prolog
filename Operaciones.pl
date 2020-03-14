7efc0f6f81600c1357158700b38202fc009ac145
Fabian Franco <fabito.10@hotmail.com>
2020-03-14 17:20:59 -0500

    Ejemplo de una calculadora en Prolog

---
+++ Operaciones.pl
@@ -0,0 +1,18 @@
+%Presentado por: Fabian Alexander Franco Quiroga - 20152020048
+%Ejemplo de prolog de una calculadora.
+%
+operaciones:-write('Ingrese el primer numero= '),
+      read(A),
+      write('Ingrese el segundo numero= '),
+      read(B),
+      C is A+B,
+      D is A-B,
+      E is A*B,
+      F is A/B,
+      format('El resultado de la suma es ~w',[C]),
+      format('El resultado de la resta es ~w',[D]),
+      format('El resultado de la multiplicacion es ~w',[E]),
+      format('El resultado de la division es ~w',[F]).
+
+
+
